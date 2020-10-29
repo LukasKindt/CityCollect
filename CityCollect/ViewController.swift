59386477724bae4c1b6214ae376d6d644debffc0
Lukas Kindt <lukas@Lukass-MacBook-Pro.local>
2020-10-29 20:20:06 +0100

    First commit

---
+++ CityCollect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityCollect
+//
+//  Created by Lukas Kindt on 29/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
